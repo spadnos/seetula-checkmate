3c910b33de3af5bf29be70018572be95bdbed9b5
Steve Padnos <spadnos@tanzanitesi.com>
2024-09-09 10:37:33 -0700

    Add dashboard page.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,45 @@
-function About() {
+"use client";
+
+import { title } from "@/components/primitives";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import Link from "next/link";
+import { ReactNode } from "react";
+
+function FeatureCard({
+  title,
+  description,
+}: {
+  title: ReactNode;
+  description: string;
+}) {
   return (
-    <div className="mt-8 flex flex-col items-center">
-      <h2 className="text-2xl font-bold">Dashboard</h2>
-      <p className="mt-4 last:max-w-3xl">
-        this page is private. You should only be able to see it if you are
-        authenticated.
-      </p>
-    </div>
+    <Card>
+      <CardHeader>{title}</CardHeader>
+      <CardContent>
+        <p>{description}</p>
+      </CardContent>
+    </Card>
   );
 }
-export default About;
+
+export default function Dashboard() {
+  return (
+    <section className=" mx-auto flex flex-col items-center">
+      <h1 className={title()}>Dashboard</h1>
+
+      <div className="mt-16">
+        <h2 className="text-2xl font-bold capitalize">Mini Apps</h2>
+        <div className="grid grid-cols-3 mt-4 w-full justify-left gap-4">
+          <FeatureCard
+            title={<Link href="/checklists">Checklists</Link>}
+            description={`More than just a simple checklist`}
+          />
+          <FeatureCard
+            title={<Link href="/polls">Polls</Link>}
+            description={`Rank choice voting.`}
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
