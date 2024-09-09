d412414713061f33a8af44db56b3bcd0c860ad14
Steve Padnos <spadnos@tanzanitesi.com>
2024-09-09 10:47:02 -0700

    Add light/dark mode

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,16 @@
 import { MainNav } from "@/components/main-nav";
 import UserButton from "@/components/user-button";
+import DarkMode from "./navbar/dark-mode";
 
 export default function Header() {
   return (
-    <header className="sticky w-full flex justify-center border-b max-w-5xl">
-      <div className="mx-auto flex h-16 w-full max-w-3xl items-center justify-between px-4 sm:px-6">
+    <header className="sticky w-full flex justify-center border-b max-w-7xl">
+      <div className="mx-auto flex h-16 w-full max-w-5xl items-center justify-between px-4 sm:px-6">
         <MainNav />
-        <UserButton />
+        <div className="flex space-x-4">
+          <DarkMode />
+          <UserButton />
+        </div>
       </div>
     </header>
   );
